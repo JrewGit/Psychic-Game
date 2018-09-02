19a7503a3613397ccbc5f71416a7337533caa6e4
Jrew <jrewmohamed@gmail.com>
2018-09-02 19:34:48 -0400

    Added reset feature

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,6 @@ var lossCounter = 0;
 var lives = 9;
 var guessList = [];
 var letterOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var computerChoice = Math.floor(Math.random() * 26);
-var computerGuess = letterOptions[computerChoice];
 
 var updateWinCounter = document.getElementById("wins");
 var updateLossCounter = document.getElementById("losses");
@@ -12,27 +10,35 @@ var updateLivesCounter = document.getElementById("livesLeft");
 var updateGuessList = document.getElementById("guessesSoFar");
 var updateWinStatement = document.getElementById("winStatement");
 var updateLossStatement = document.getElementById("lossStatement");
+var computerChoice = Math.floor(Math.random() * 26);
+var computerGuess = letterOptions[computerChoice];
 
-document.onkeyup = function () {
+function resetGame() {
+  lives = 9;
+  updateLivesCounter.textContent = lives;
+  guessList = [];
+  updateGuessList.textContent = guessList;
+  computerChoice = Math.floor(Math.random() * 26);
+  computerGuess = letterOptions[computerChoice];
+}
 
-    var userGuess = event.key;
-    console.log(computerGuess);
-    if (userGuess !== computerGuess) {
-        if (lives > 0 && winCounter !== 1) {
-            lives -= 1;
-            guessList.push(userGuess);
-            updateLivesCounter.textContent = lives;
-            updateGuessList.innerHTML = guessList;
-        } if (lives === 0) {
-            lossCounter = 1;
-            updateLossCounter.textContent = lossCounter;
-            //   updateLossStatement.textContent = "Oooo so closee!!! Refresh your page to play again!"
-        }
-    } else if (lossCounter !== 1) {
-        winCounter = 1;
-        updateWinCounter.textContent = winCounter;
-
-        // updateWinStatement.textContent = "You did it!!! Refresh your page to play again!";
+document.onkeyup = function(event) {
+  var userGuess = event.key;
+  console.log(computerGuess);
+  if (userGuess !== computerGuess) {
+    if (lives > 0) {
+      lives -= 1;
+      guessList.push(userGuess);
+      updateLivesCounter.textContent = lives;
+      updateGuessList.innerHTML = guessList;
+    } if (lives === 0) {
+      lossCounter += 1;
+      updateLossCounter.textContent = lossCounter;
+      resetGame();
     }
-
+  } else {
+    winCounter += 1;
+    updateWinCounter.textContent = winCounter;
+    resetGame();
+  }
 }
